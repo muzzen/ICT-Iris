83a9590db9878763a749af1cf146b377dc8265ab
muzzen <68534325+muzzen@users.noreply.github.com>
2023-11-24 15:00:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,10 @@ def home():
 @app.route('/result', methods=['GET','POST'])
 def result():
     if request.method == 'POST':
-        sepal_length = float(request.form.get('sepal_length'))
-        sepal_width = float(request.form.get('sepal_width'))
-        petal_length = float(request.form.get('petal_length'))
-        petal_width = float(request.form.get('petal_width'))
-
+        sepal_length = float(request.form['sepal_length'])
+        sepal_width = float(request.form['sepal_width'])
+        petal_length = float(request.form.['petal_length'])
+        petal_width = float(request.form.['petal_width'])
 
         model = pickle.load(open('classifier.pkl','rb'))
     
